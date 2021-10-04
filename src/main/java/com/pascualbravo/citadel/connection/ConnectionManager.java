d1ad9e535addcfa3583dd41877ef7ce107faf8b3
higuitamartinez <higuitamartinez@gmail.com>
2021-10-04 14:31:58 -0500

    Arreglando rutas

--- src/main/java/com/pascualbravo/citadel/connection/ConnectionManager.java
+++ src/main/java/com/pascualbravo/citadel/connection/ConnectionManager.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    private static final String URL = "jdbc:postgresql://localhost:5432/bookshop_citadel";
-    private static final String USER="postgres";
-    private static final String PASSWORD="342940";
+    private static final String URL = "jdbc:postgresql://localhost:your_port/bookshop_citadel";
+    private static final String USER="your_user";
+    private static final String PASSWORD="your_pass";
     
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL, USER, PASSWORD);
